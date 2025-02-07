51018934e90d4fe7ea4de79acf84676215aa0872
Sean Kepner <citrusflavored@gmail.com>
2025-02-06 22:13:52 -0500

    Comments on main game logic

--- src/bashjack.sh
+++ src/bashjack.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 # TODO: Add the rules of blackjack as steps to complete to scope out project reqs
+
+
+# welcome screen
+# menu_loop entry point -> exit -x or new_game -> collect_settings_loop
+# initialize game variables: players, dealer, chips
+# initialize table: deck
+# game_loop
+#   player_turn
+#     player_action_loop
+#       hit|stand
+
+# dealer_turn
+#   dealer_action_loop
+#     hit|stand
+
+# check_for_winner -> calculate_hand_value -> compare_hands -> announce_winner -> update_chips
